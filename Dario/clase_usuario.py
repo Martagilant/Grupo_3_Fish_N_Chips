077c55deeee6e46cecd0bea97de3e37fd2949c06
DarioRuizGon <darioruiz334@gmail.com>
2025-03-28 01:01:11 +0100

    Bucle perfeccionado

--- Dario/clase_usuario.py
+++ Dario/clase_usuario.py
@@ -15,6 +15,7 @@ class Usuario:
         self.tablero_barcos = np.full((10,10), " ") #Falta importar clase tablero y poner un tablero creado ahí
         self.tablero_disparos = np.full((10,10), " ") #Falta importar clase tablero y poner un tablero creado ahí
         self.vidas = self.calcula_vidas()
+        self.terminar = False
 
 
     def coloca_barcos_aleatorio(self):
@@ -32,45 +33,50 @@ class Usuario:
         if jugador.tablero_barcos[ubicacion[0], ubicacion[1]] == "O":
             jugador.tablero_barcos[ubicacion[0], ubicacion[1]] = "X" #Marca el acierto en el tablero de barcos del jugador
             self.tablero_disparos[ubicacion[0], ubicacion[1]] = "X"
+            print("¡Tocado!\nLa máquina te ha dado")
             resultado = True #Marca el acierto en el tablero de disparos de la máquina
             return resultado #Para controlar el bucle while en el que irá incluido y saber si sigue disparando o no
         else:
             jugador.tablero_barcos[ubicacion[0], ubicacion[1]] = "-" #Marca el fallo
             self.tablero_disparos[ubicacion[0], ubicacion[1]] = "-"
+            print("¡Agua! La máquina es casi tan mala como tú")
             resultado = False
             return resultado
         
 
     def disparo_jugador(self):
         coordenada_1 = input("Introduce la fila (del 1 al 10): ") #Pide las coordenadas una a una para una conversión más sencilla
-        coordenada_1
-        while int(coordenada_1) > 10 or int(coordenada_1) < 1: #Verifica que se haya metido una coordenada válida y si no la vuelve a pedir
-            print("La coordenada no puede ser mayor de 10")
-            coordenada_1 = input("Introduce la fila (del 1 al 10): ")
-        coordenada_2 = input("Introduce la columna (del 1 al 10): ")
-        coordenada_2
-        while int(coordenada_2) > 10 or int(coordenada_1) < 1:
-            print("La coordenada no puede ser mayor de 10")
-            coordenada_2 = input("Introduce la columna: ")
-        ubicacion = [int(coordenada_1)-1, int(coordenada_2)-1] 
-
-        if maquina.tablero_barcos[ubicacion[0], ubicacion[1]] == "O": #mismo mecanismo que disparo máquina
-            maquina.tablero_barcos[ubicacion[0], ubicacion[1]] = "X"
-            self.tablero_disparos[ubicacion[0], ubicacion[1]] = "X"
-            print(jugador.tablero_disparos)
-            resultado = True
-            return resultado
-        if maquina.tablero_barcos[ubicacion[0], ubicacion[1]] == "X" or maquina.tablero_barcos[ubicacion[0], ubicacion[1]] == "-": #avisa al jugador si ya ha disparado ahí pero el turno lo pierde igual
-            print("Ya has disparado ahí")
-            print(jugador.tablero_disparos)
-            resultado = False
-            return resultado
+        if coordenada_1 == "terminar":
+            jugador.terminar = True
+            print("Has elegido terminar el juego")
         else:
-            maquina.tablero_barcos[ubicacion[0], ubicacion[1]] = "-"
-            self.tablero_disparos[ubicacion[0], ubicacion[1]] = "-"
-            print(jugador.tablero_disparos)
-            resultado = False
-            return resultado
+            coordenada_1 = int(coordenada_1)
+        if type(coordenada_1) == int:
+            while coordenada_1 > 10 or coordenada_1 < 1: #Verifica que se haya metido una coordenada válida y si no la vuelve a pedir
+                print("La coordenada no puede ser mayor de 10")
+                coordenada_1 = input("Introduce la fila (del 1 al 10): ")
+            coordenada_2 = input("Introduce la columna (del 1 al 10): ")
+            while int(coordenada_2) > 10 or int(coordenada_1) < 1:
+                print("La coordenada no puede ser mayor de 10")
+                coordenada_2 = input("Introduce la columna: ")
+            ubicacion = [int(coordenada_1)-1, int(coordenada_2)-1] 
+
+            if maquina.tablero_barcos[ubicacion[0], ubicacion[1]] == "O": #mismo mecanismo que disparo máquina
+                maquina.tablero_barcos[ubicacion[0], ubicacion[1]] = "X"
+                self.tablero_disparos[ubicacion[0], ubicacion[1]] = "X"
+                print(f"¡Tocado! Vaya máquina\nTu último disparo ha sido {ubicacion[0]+1}, {ubicacion[1]+1}")
+                resultado = True
+                return resultado
+            if maquina.tablero_barcos[ubicacion[0], ubicacion[1]] == "X" or maquina.tablero_barcos[ubicacion[0], ubicacion[1]] == "-": #avisa al jugador si ya ha disparado ahí pero el turno lo pierde igual
+                print("Ya has disparado ahí")
+                resultado = False
+                return resultado
+            else:
+                maquina.tablero_barcos[ubicacion[0], ubicacion[1]] = "-"
+                self.tablero_disparos[ubicacion[0], ubicacion[1]] = "-"
+                print("¡Agua! Vaya paquetón")
+                resultado = False
+                return resultado
     
     def calcula_vidas(self): #Recorre todo el array contado las O, ese es el número de vidas
         contador_vidas = 0
